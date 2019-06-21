ea38174b956f0e2ff1e59b890e8c11021836eb89
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-21 15:00:08 +0700

    add initial HumanCharacter model

---
+++ MarvelOO/Models/HumanCharacter.swift
@@ -0,0 +1,22 @@
+//
+//  HumanCharacter.swift
+//  MarvelOO
+//
+//  Created by PT. GOJEK INDONESIA on 21/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import Foundation
+
+class Human: Character {
+    var health: Int = 100
+    var damage: Int = 10
+    var abilities: [String] = []
+    var name: String
+    var description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}
