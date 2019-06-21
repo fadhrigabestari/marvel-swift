b8ca7f52910ede4a13e5d22c9195553876f721d6
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-21 16:20:12 +0700

    add constant file

---
+++ MarvelOO/CharacterConstants.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterConstants.swift
+//  MarvelOO
+//
+//  Created by PT. GOJEK INDONESIA on 21/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import Foundation
+
+let BASIC_HEALTH = 100
+let BASIC_DAMAGE = 10
+
+let 
