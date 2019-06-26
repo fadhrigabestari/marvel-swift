4ea51209506f582d7cbebb262fd28dbc72786db2
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-26 18:25:14 +0700

    add unit testing for alien character

---
+++ MarvelOOTests/CharacterTests/AlienCharacterTest.swift
@@ -0,0 +1,32 @@
+//
+//  AlienCharacterTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 26/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class AlienCharacterTest: XCTestCase {
+    
+    var character: AlienCharacter!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        character = AlienCharacter(name: "fadh", description: "testing", origin: "bumi")
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        character = nil
+    }
+    
+    func shouldAlienate() {
+        XCTAssertEqual(character.health, 150)
+        XCTAssertEqual(character.damage, 105)
+        XCTAssertEqual(character.abilities[0], "Alien")
+    }
+}
