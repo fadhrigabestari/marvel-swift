afc9e38fc9342196edd3239c447ed28d26016579
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-26 18:28:28 +0700

    add unit testing for beast character

---
+++ MarvelOOTests/CharacterTests/BeastCharacterTest.swift
@@ -0,0 +1,32 @@
+//
+//  BeastCharacterTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 26/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class BeastCharacterTest: XCTestCase {
+    
+    var character: BeastCharacter!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        character = BeastCharacter(name: "fadh", description: "testing", species: "monyed")
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        character = nil
+    }
+    
+    func shouldBeastify() {
+        XCTAssertEqual(character.health, 80)
+        XCTAssertEqual(character.damage, 30)
+        XCTAssertEqual(character.abilities[0], "Beast")
+    }
+}
