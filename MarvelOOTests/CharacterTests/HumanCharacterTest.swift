d5fb6b711e2ecb08cd244008b21715737c4ef0cd
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-26 18:07:03 +0700

    add unit testing for human character

---
+++ MarvelOOTests/CharacterTests/HumanCharacterTest.swift
@@ -0,0 +1,28 @@
+//
+//  HumanCharacterTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 26/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class HumanCharacterTest: XCTestCase {
+    
+    var character: HumanCharacter!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        character = HumanCharacter(name: "Fadhriga", description: "Unit testing")
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        character = nil
+    }
+
+    func shouldHumanize() {
+        XCTAssertEqual(character.abilities[0], "Human")
+    }
+}
