67e5f090d075dc9100a2f29696fe8add2a8efc66
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-26 18:43:58 +0700

    add unit testing for beast+alien character

---
+++ MarvelOOTests/CharacterTests/Beast+AlienCharacterTest.swift
@@ -0,0 +1,32 @@
+//
+//  Beast+AlienCharacterTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 26/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class BeastAlienCharacterTest: XCTestCase {
+    
+    var character: BeastAlienCharacter!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        character = BeastAlienCharacter(name: "fadh", description: "testing", origin: "bumi", species: "monyed")
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        character = nil
+    }
+
+    func shouldAlienateAndBeastify() {
+        XCTAssertEqual(character.health, 115)
+        XCTAssertEqual(character.damage, 22)
+        XCTAssertEqual(character.abilities, ["Half-alien", "Half-beast"])
+    }
+}
