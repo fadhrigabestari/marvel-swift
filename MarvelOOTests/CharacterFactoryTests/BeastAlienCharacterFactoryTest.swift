0440455c7a89c3479db9d63bd626bbd6dddb896e
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-27 11:27:49 +0700

    add unit testing for beast alien character factory

---
+++ MarvelOOTests/CharacterFactoryTests/BeastAlienCharacterFactoryTest.swift
@@ -0,0 +1,31 @@
+//
+//  BeastAlienCharacterFactoryTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 27/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class BeastAlienCharacterFactoryTest: XCTestCase {
+    
+    var factory: BeastAlienCharacterFactory!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        factory = BeastAlienCharacterFactory()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        factory = nil
+    }
+
+    func testManufacture() {
+        let character = factory.manufacture()
+        XCTAssert(character is BeastAlienCharacter)
+    }
+}
