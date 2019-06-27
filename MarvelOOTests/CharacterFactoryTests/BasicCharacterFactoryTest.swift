deb7fb57c904ad3e0f16b899940a9930bafd998f
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-27 11:29:05 +0700

    add unit testing for basic character factory

---
+++ MarvelOOTests/CharacterFactoryTests/BasicCharacterFactoryTest.swift
@@ -0,0 +1,31 @@
+//
+//  BasicCharacterFactoryTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 27/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class BasicCharacterFactoryTest: XCTestCase {
+    
+    var factory: BasicCharacterFactory!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        factory = BasicCharacterFactory()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        factory = nil
+    }
+
+    func testManufacture() {
+        let character = factory.manufacture()
+        XCTAssert(character is BasicCharacter)
+    }
+}
