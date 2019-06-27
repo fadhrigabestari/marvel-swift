400fb8e8c8b4e9faac9c1bec7eda190129fb44a8
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-06-27 11:57:06 +0700

    add unit testing for status action

---
+++ MarvelOOTests/HandlerTests/StatusActionTest.swift
@@ -0,0 +1,32 @@
+//
+//  StatusActionTest.swift
+//  MarvelOOTests
+//
+//  Created by PT. GOJEK INDONESIA on 27/06/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import XCTest
+
+class StatusActionTest: XCTestCase {
+    
+    var action: StatusAction!
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        let printer = Printer()
+        action = StatusAction(printer: printer)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        action = nil
+    }
+    
+    func testExecute() {
+        let character = HumanCharacter(name: "fadh", description: "testing")
+        XCTAssertFalse(action.execute(by: character))
+    }
+}
