2e92c133a4b5628969e64fcbf7312f9564d0cb81
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-07-03 14:59:31 +0700

    move game functionalities to main

--- MarvelOO/main.swift
+++ MarvelOO/main.swift
@@ -13,13 +13,75 @@ let gameHandler = GameHandler(inputManager: CommandLineInputManager(),
                           printer: Printer(),
                           factory: BasicCharacterFactory(inputManager: CommandLineInputManager()),
                           action: NoAction(printer: Printer()))
-let isPlayerOneTurn = true
-let isGameOver = false
 
-var game = MarvelGame(handler: gameHandler, isPlayerOneTurn: isPlayerOneTurn, isGameOver: isGameOver)
-
+var isPlayerOneTurn = true
+var isGameOver = false
 var playerOne: Character
 var playerTwo: Character
 
-(playerOne, playerTwo) = game.setup()
-game.gameLoop(playerOne: playerOne, playerTwo: playerTwo)
+// Game functions
+func setup() -> (Character, Character) {
+    let playerOne = characterCreation()
+    let playerTwo = characterCreation()
+    
+    return (playerOne: playerOne, playerTwo: playerTwo)
+}
+
+func gameLoop(playerOne: Character, playerTwo: Character) {
+    while !isGameOver {
+        announceTurn()
+        gameHandler.printer.printGameLoop()
+        
+        let input = gameHandler.inputManager.getInputString()
+        let shouldReverseTurn = gameHandler.handleInputInGame(input: input,
+                                                              playerOne: playerOne,
+                                                              playerTwo: playerTwo,
+                                                              isPlayerOneTurn: isPlayerOneTurn)
+        checkGameOver(playerOne: playerOne, playerTwo: playerTwo)
+        
+        if shouldReverseTurn {
+            reverseTurn()
+        }
+        
+        if isGameOver {
+            reverseTurn()
+        }
+    }
+    announceWinner()
+}
+
+func characterCreation() -> Character {
+    announceTurn()
+    gameHandler.printer.printCharacterCreation()
+    let input = gameHandler.inputManager.getInputInt()
+    gameHandler.factory = gameHandler.factory.determineFactory(inputManager: CommandLineInputManager(), option: input)
+    let character = gameHandler.factory.manufacture()
+    reverseTurn()
+    
+    return character
+}
+
+func announceTurn() {
+    let message = isPlayerOneTurn ? "PLAYER ONE'S TURN" : "PLAYER TWO'S TURN"
+    print(message)
+}
+
+func reverseTurn() {
+    isPlayerOneTurn = !isPlayerOneTurn
+}
+
+func checkGameOver(playerOne: Character, playerTwo: Character) {
+    if playerOne.health <= 0 || playerTwo.health <= 0 {
+        isGameOver = true
+    }
+}
+
+func announceWinner() {
+    let message = isPlayerOneTurn ? "PLAYER ONE WINS" : "PLAYER TWO WINS"
+    print(message)
+}
+
+// main
+(playerOne, playerTwo) = setup()
+gameLoop(playerOne: playerOne, playerTwo: playerTwo)
+
